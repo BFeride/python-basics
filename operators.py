ff2865040cbeb4245e599626d83fdd36509731b3
Bebirli Feride <bferide0808@gmail.com>
2026-08-04 22:28:30 +0400

    Add operators.py

---
+++ operators.py
@@ -0,0 +1,16 @@
+deyisen1 = 25
+deyisen2 = 8
+
+print("Arithmetic Operators")
+print("-" * 25)
+print(f"toplama: {deyisen1} + {deyisen2} = {deyisen1 + deyisen2}")
+print(f"çixma: {deyisen1} - {deyisen2} = {deyisen1 - deyisen2}")
+print(f"vurma: {deyisen1} * {deyisen2} = {deyisen1 * deyisen2}")
+print(f"bölme: {deyisen1} / {deyisen2} = {deyisen1 / deyisen2:.2f}")
+print(f"qaliq: {deyisen1} % {deyisen2} = {deyisen1 % deyisen2}")
+print(f"quvvet: {deyisen1} ** {deyisen2} = {deyisen1 ** deyisen2}")
+
+print("\nComparison Operators")
+print(f"{deyisen1} > {deyisen2}: {deyisen1 > deyisen2}")
+print(f"{deyisen1} == {deyisen2}: {deyisen1 == deyisen2}")
+print(f"{deyisen1} != {deyisen2}: {deyisen1 != deyisen2}")

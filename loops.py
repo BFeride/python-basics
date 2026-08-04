d29be289f3b6da869bfd6dd5ce679f8ec525997a
Bebirli Feride <bferide0808@gmail.com>
2026-08-04 22:44:59 +0400

    Add loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+print("For Loop")
+for number in range(1, 11):
+    print(number)
+
+print("\nEven Numbers")
+
+for number in range(2, 21, 2):
+    print(number)
+
+print("\nWhile Loop")
+
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Finished!")

ebe870e0086ac8678ad65a4db46d296f309e89a1
Bebirli Feride <bferide0808@gmail.com>
2026-08-04 22:35:19 +0400

    Add if_else.py

---
+++ if_else.py
@@ -0,0 +1,23 @@
+bal = 87
+
+if bal >= 90:
+    qiymet = "A"
+elif bal >= 80:
+    qiymet = "B"
+elif bal >= 70:
+    qiymet = "C"
+elif bal >= 60:
+    qiymet = "D"
+else:
+    qiymet = "F"
+
+print(f"bal: {bal}")
+print(f"qiymet: {qiymet}")
+
+yas = 18
+
+if yas >= 18:
+    print("uygundur")
+else:
+    print("uygun deyil")
+
